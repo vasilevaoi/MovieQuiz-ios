4c2cfacbd9bdd1d4e1fc67f448ac3da15b44838d
olga-vasileva <voi1992@yandex.ru>
2023-07-03 23:09:26 +0400

    исправлен цвет статус бара

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -18,6 +18,9 @@ final class MovieQuizViewController: UIViewController {
     private var alertPresenter: AlertPresenter?
     private var statisticService: StatisticService?
     // MARK: - Lifecycle
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+      return .lightContent
+     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
